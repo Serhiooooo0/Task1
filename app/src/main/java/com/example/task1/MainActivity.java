6f62f7810039d5a4e58a5ccd435b452beaf7f747
rmp <asdasdasd>
2023-09-28 13:08:25 +0300

    reasresd

--- app/src/main/java/com/example/task1/MainActivity.java
+++ app/src/main/java/com/example/task1/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.task1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View v){
+        Intent intent = new Intent(this,OnBoard.class);
+        startActivity(intent);
+    }
 }
+
